a0e5f0b8bd6c2bb1d5c8a120b840af586caafa3c
OussamaGhamian <oussamaalghamian@gmail.com>
2020-03-30 17:56:24 +0300

    /api/currencies backend

--- Savor/app/Http/Controllers/CurrencyController.php
+++ Savor/app/Http/Controllers/CurrencyController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Currency;
 
 class CurrencyController extends Controller
 {
-    //
+    function index()
+    {
+        $currencies = Currency::all();
+        return $currencies;
+    }
+    // function show($id)
+    // {
+    //     $currency = Currency::findOrFail($id);
+    //     return $currency;
+    // }
 }
